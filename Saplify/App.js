73b54460a579c7f32027db82acd602422c176c33
Julian <vecera.julian@gmail.com>
2022-02-09 17:27:42 +0100

    add App.js

--- Saplify/App.js
+++ Saplify/App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.boxImage}>
+        HERE
+        <Image
+        source={require('./assets/images/sap1.png')}
+        style={styles.image}
+        />
+
+      </View>
+      <View style={styles.boxText}>
+        <Text>Price: xx kr</Text>
+        <Text>Description: here comes the description, it can be quite long and must be handled as a long text</Text>
+        <StatusBar style="auto" />
+      </View>
+
+      
     </View>
   );
 }
@@ -13,8 +27,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: '100%',
+    height: '100%'
+  },
+  boxImage: {
+    flex: 2,
+  },
+  boxText: {
+    flex: 2
+  }
 });
