7059064d1f52694ea3b5d5c5435a1a7bdd2c8587
octave LE TULLIER <octave.letullier@telecom-paris.fr>
2022-02-11 12:43:55 +0100

    Add drawer

--- Saplify/App.js
+++ Saplify/App.js
@@ -1,21 +1,21 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-//import { createDrawerNavigator } from '@react-navigation/drawer';
+//import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import HomeScreen from './screens/HomeScreen';
+import HomeStackScreen from './screens/HomeStackScreen';
 import AboutScreen from './screens/AboutScreen';
 import NewItemScreen from './screens/NewItemScreen';
-import DetailedViewScreen from './screens/DetailedViewScreen';
+import HomeHeader from './components/HomeHeader';
 
-const Stack = createStackNavigator();
-//const Drawer = createDrawerNavigator();
+//const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function App() {
-  const ref = React.useRef(null);
+  const [isSearching, setIsSearching] = React.useState(false);
   return (
-    <View style={{ flex: 1 }}>
+/*     <View style={{ flex: 1 }}>
       <NavigationContainer ref={ref}>
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="About" component={AboutScreen} />
@@ -24,13 +24,28 @@ function App() {
           <Stack.Screen name="NewItem" component={NewItemScreen} />
         </Stack.Navigator>
       </NavigationContainer>
-    </View>
-/*         <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Home">
-          <Drawer.Screen name="Home" component={HomeScreen} />
-          <Drawer.Screen name="About" component={AboutScreen} />
+    </View> */
+        <NavigationContainer>
+        <Drawer.Navigator initialRouteName="HomeStack">
+          <Drawer.Screen name="HomeStack" 
+                                          options={{
+                                                title: 'Sapling',
+                                                headerStyle: {
+                                                  backgroundColor: '#9CD353',
+                                                },
+                                                headerTintColor: '#fff',
+                                                headerTitleStyle: {
+                                                  fontWeight: 'bold',
+                                                },
+                                              }} 
+                                          component={HomeStackScreen} />
+          <Drawer.Screen name="About"
+                         component={AboutScreen}
+                         //options={{ headerTitle: () => <HomeHeader isSearching={isSearching} setIsSearching={setIsSearching}/>}} 
+                         />
+          <Drawer.Screen name="NewItem" component={NewItemScreen} />
         </Drawer.Navigator>
-      </NavigationContainer> */
+      </NavigationContainer>
   );
 }
 
--- Saplify/screens/AboutScreen.js
+++ Saplify/screens/AboutScreen.js
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-const AboutScreen = ({ navigation }) => {
+const AboutScreen = ({ navigation, route }) => {
         return (
             <View>
 
                 <Button onPress={() => navigation.navigate('Home')} title="go">
                 </Button>
                 <View>
-                    <Text style={styles.titleText}>About</Text>
+                    <Text style={styles.titleText}>About us</Text>
                     <Text style={styles.baseText}>{"\n"}Saplify is an app to buy and sell Saplings easily. It's focus is on providing an online marketplace and letting people create offers and let buyers contact them.{"\n"}{"\n"}</Text>
                     <Text style={styles.baseText}>Made by AppDevTeam / Group 7 as project in Developing Mobile Applications</Text>
                     <StatusBar style="auto" />
