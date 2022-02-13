14884528bf208879549a1df3c4d7bccb336cf335
William <Williamaxb@gmail.com>
2022-02-13 17:49:43 +0100

    Connected to the cloud now

--- Saplify/screens/NewItemScreen.js
+++ Saplify/screens/NewItemScreen.js
@@ -4,9 +4,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Button, TextInput, Pressable } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import {styles} from "../assets/StyleSheet.js";
+import { Plant } from "../components/Plant.js";
 
 const NewItemScreen = ({ navigation }) => {
     const [image, setImage] = useState(null);
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [location, setLocation] = useState('');
+    const [description, setDescription] = useState('');
 
     const pickImage = async () => {
       // No permissions request is necessary for launching the image library
@@ -16,46 +23,56 @@ const NewItemScreen = ({ navigation }) => {
         aspect: [4, 3],
         quality: 1,
       });
-
       console.log(result);
-
       if (!result.cancelled) {
         setImage(result.uri);
       }
     };
 
     const uploadPicture = ()=> {
+      const plant = new Plant("1", name, price, phone, email, location, description, image);
+      plant.postData();
       console.log("TODO: Create backend")
     }
 
         return (
             <View style={styles.container}>
-            <Text style={styles.title}>
-
-            New Plant</Text>
+            <Text style={styles.title}>New Plant</Text>
                 <View style={styles.newItemBox}>
                     <Text style={styles.newItemHeader}>Plant:</Text>
                     <TextInput
+                        defaultValue={name}
+                        onChangeText={newText => setName(newText)}
                         style={styles.newItemInput}
                     />
                     <Text style={styles.newItemHeader}>Price:</Text>
                     <TextInput
+                        defaultValue={price}
+                        onChangeText={newText => setPrice(newText)}
                         style={styles.newItemInput}
                     />
                     <Text style={styles.newItemHeader}>Phone:</Text>
                     <TextInput
+                        defaultValue={phone}
+                        onChangeText={newText => setPhone(newText)}
                         style={styles.newItemInput}
                     />
                     <Text style={styles.newItemHeader}>E-Mail:</Text>
                     <TextInput
+                        defaultValue={email}
+                        onChangeText={newText => setEmail(newText)}
                         style={styles.newItemInput}
                     />
                     <Text style={styles.newItemHeader}>Location:</Text>
                     <TextInput
+                        defaultValue={location}
+                        onChangeText={newText => setLocation(newText)}
                         style={styles.newItemInput}
                     />
                     <Text style={styles.newItemHeader}>Description:</Text>
                     <TextInput
+                        defaultValue={description}
+                        onChangeText={newText => setDescription(newText)}
                         style={styles.newItemInput}
                     />
                     <Pressable onPress={pickImage} style={styles.newItemAddPicture}>
