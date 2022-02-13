f61fa1a16ff0ace0583fa0e4f31f92f2f07cc959
William <Williamaxb@gmail.com>
2022-02-13 17:47:40 +0100

    Changed provided parameters

--- Saplify/components/PlantsList.js
+++ Saplify/components/PlantsList.js
@@ -2,11 +2,10 @@ import * as React from 'react';
 import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function({item, navigation}) {
-
     return <View key={item.key} >
               <TouchableOpacity
                   onPress={() => navigation.navigate('DetailedView',
-                  {price: item.price, description: item.description, imageUrl: item.imageUrl, name: item.name, contact: item.contact})}
+                  {price: item.price, description: item.description, imageUrl: item.imageUrl, name: item.name, phone: item.phone, email: item.email, location: item.location})}
                   style={styleSheet.gridbox}>
                     <View style={styleSheet.imagebox}>
 
@@ -35,7 +34,7 @@ export default function({item, navigation}) {
                       <View style={styleSheet.descriptionbox}>
                           <Text  numberOfLines={3}  style = {styleSheet.description}>
                               Contact:
-                              {"\n"} {item.contact}
+                              {"\n"} {item.phone}
                           </Text>
                       </View>
                   </View>
